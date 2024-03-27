2b6bd1dc622a2fc5f4810ac7ed14c023ea8eb423
trongtai <trongtai.nguyen789@gmail.com>
2024-03-27 11:25:15 +0700

    create base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Select } from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +19,19 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Select
+        options={[
+          {
+            value: 1,
+            label: "abcd",
+          },
+          {
+            value: 2,
+            label: "abcd",
+          },
+        ]}
+      ></Select>
     </div>
   );
 }
